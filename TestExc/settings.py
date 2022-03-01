ae09d10b2999f14bc5359d9d89efd873f547534f
Melancholia312 <67428510+Melancholia312@users.noreply.github.com>
2022-03-01 18:32:32 +0300

    Update settings.py

--- TestExc/settings.py
+++ TestExc/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_exc',
         'USER': 'postgres',
-        'PASSWORD': 'TombRaider2003',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
